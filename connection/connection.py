02460f07cf3be71d3c23e21c04b924d83267b33d
flotes3 <43554109+flotes3@users.noreply.github.com>
2022-10-28 19:29:44 +0200

    Finished first clientConnection.py version

--- connection/connection.py
+++ connection/connection.py
@@ -99,3 +99,39 @@ class Connection:
             "type": "error",
             "error ": error
         })
+
+    # Send the messages needed at the start of a connection
+    def send_initial_messages(self) -> bool:
+        # Send hello and get_peers
+        if not self.send_hello():
+            utils.printer.printout("Couldn't send the hello message!")
+            return False
+        if not self.send_get_peers():
+            utils.printer.printout("Couldn't send the get peers message!")
+            return False
+        return True
+
+    # Loop trough new messages and answer them
+    def maintain_connection(self) -> None:
+        hello_received = False
+
+        # Loop trough new received messages
+        while True:
+            msgs = self.receive_msg()
+
+            for i in msgs:
+                if not i["type"]:
+                    self.send_error("No valid message received")
+
+                if i["type"] == "hello":
+                    hello_received = self.receive_hello(i)
+
+                if not hello_received:
+                    self.send_error("Sent no hello message at start of conversation.")
+                    return
+
+                if i["type"] == "getpeers":
+                    self.send_peers()
+
+                if i["type"] == "peers":
+                    self.receive_peers()
--- connection/serverConnection.py
+++ connection/serverConnection.py
@@ -3,38 +3,14 @@ import utils
 from connection.connection import Connection
 
 
+# Server to handle incoming connections
+
 class ServerConnection(Connection):
 
     def handle_client(self) -> None:
         utils.printer.printout(f"[NEW CONNECTION] {self.addr} connected.")
 
-        # Send hello and get_peers
-        if not self.send_hello():
-            utils.printer.printout("Couldn't send the hello message!")
-            return
-        if not self.send_get_peers():
-            utils.printer.printout("Couldn't send the get peers message!")
-            return
+        self.send_initial_messages()
+        self.maintain_connection()
 
-        hello_received = False
 
-        # Loop trough new received messages
-        while True:
-            msgs = self.receive_msg()
-
-            for i in msgs:
-                if not i["type"]:
-                    self.send_error("No valid message received")
-
-                if i["type"] == "hello":
-                    hello_received = self.receive_hello(i)
-
-                if not hello_received:
-                    self.send_error("Sent no hello message at start of conversation.")
-                    return
-
-                if i["type"] == "getpeers":
-                    self.send_peers()
-
-                if i["type"] == "peers":
-                    self.receive_peers()
