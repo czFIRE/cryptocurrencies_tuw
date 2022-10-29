71719e7d92c149531159cf8dd40ef193bb1e80ff
Petr Kadlec <xkadlec6@fi.muni.cz>
2022-10-29 18:09:50 +0200

    Added proper saving functionality

--- utils.py
+++ utils.py
@@ -3,7 +3,10 @@ from sys import stdout
 from typing import Dict, TextIO
 from datetime import date, datetime
 
-import json
+from time import sleep
+from threading import Lock, Thread
+
+import pickle
 import os.path
 
 
@@ -36,28 +39,67 @@ class Printer:
 class PeerSaver:
     '''Used for saving and loading discovered peers'''
 
+    peer_lock = Lock()
+
     # handle file overwriting in a nice way
     def __init__(self, file_location: str) -> None:
         self.peers: Dict = {}
         self.file_location = file_location
+
+        daemon = Thread(target=self.auto_save, args=(3600,), daemon=True, name='Background')
+        daemon.start()
+
+    def __del__(self) -> None:
+        self.save()
 
     def load(self) -> None:
         if (not os.path.exists(self.file_location)):
             with open(self.file_location, 'w') as file:
                 return
 
-        with open(self.file_location, 'r') as file:
-            self.peers = json.loads(file.read())
-            print(self.peers)
+        with open(self.file_location, 'rb') as file:
+            self.peers = pickle.load(file)
+            printer.printout("Loaded these peers: " + str(self.peers))
 
     def save(self) -> None:
-        with open(self.file_location, 'w') as file:
-            file.write(json.dumps(self.peers))
+        with open(self.file_location, 'wb') as file:
+            pickle.dump(self.peers, file)
 
     def add_peer(self, peer) -> None:
-        self.peers.update(peer)
+        with self.peer_lock:
+            self.peers.update(peer)
+
+    # Saves the work each hour
+    def auto_save(self, interval_sec):
+        # run forever
+        while True:
+            # block for the interval
+            sleep(interval_sec)
+            # perform the task
+            self.save()
 
 
 # Make a public instance of printer such that it is visible across the whole implementation
 printer = Printer()
-peer_saver = PeerSaver("peer_db.json")
+peer_saver = PeerSaver("peer_db.pickle")
+
+""" 
+# test if this works
+def test_load_save():
+    peer_saver.load()
+
+    print(peer_saver.peers)
+
+    for i in range(100):
+        thread = Thread(target=test_add_peer, args=(i,))
+        thread.start()
+
+    print(peer_saver.peers)
+
+    peer_saver.save()
+
+def test_add_peer(i):
+    peer_saver.add_peer({"ha" + str(i): 5})
+
+test_load_save() 
+"""
