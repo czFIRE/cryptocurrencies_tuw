33ea2dad7db7a4be6c8dcbabf3c0403ea130a1d6
Petr Kadlec <xkadlec6@fi.muni.cz>
2022-11-28 11:27:31 +0100

    Fixing some conflicts and properly setting up gitignore

--- tests/test_request.py
+++ tests/test_request.py
@@ -3,6 +3,12 @@ import random
 import time
 import unittest
 
+import os, sys
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
 from message import msg_builder
 import asyncio
 
